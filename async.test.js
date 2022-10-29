d76eb5652cc35c54a8e374fb096d18c2b98f636a
trantuvan <trantuvan.kan@gmail.com>
2022-10-29 20:42:13 +0700

    Test Async with callback based
    expect.assertions(1) makes sure 1 callback get callback.
    fail here because fetchData is a promise

--- async.test.js
+++ async.test.js
@@ -12,3 +12,16 @@ test("the data is peanut butter promise", () => {
     expect(data).toBe("peanut butter");
   });
 });
+
+test("the fetch fails with an error", async () => {
+  // *expect.assertions(1) uses when function is callback based
+  // *make sure the callback function get call later
+  // *1 here means just 1 callback function in the fetchData
+  // !expect.assertions(1);
+  // !this test will fail because fetchData is a promise call not a callback
+  try {
+    await fetchData();
+  } catch (error) {
+    expect(error).toMatch(/error/);
+  }
+});
