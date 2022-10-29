41c256452f8f9baea87e775013a0234ca622bf5b
trantuvan <trantuvan.kan@gmail.com>
2022-10-29 20:31:07 +0700

    test fetchData with async

--- async.test.js
+++ async.test.js
@@ -2,7 +2,12 @@ const fetchData = () => {
   return Promise.resolve("peanut butter");
 };
 
-test("the data is peanut butter", () => {
+test("the data is peanut butter async", async () => {
+  const data = await fetchData();
+  expect(data).toBe("peanut butter");
+});
+
+test("the data is peanut butter promise", () => {
   fetchData().then((data) => {
     expect(data).toBe("peanut butter");
   });
